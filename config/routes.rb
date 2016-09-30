14b10043210719d2b3d02a0ebb5ed5a022c9b79b
Graeme Nathan <graeme@stembolt.com>
2016-09-30 14:51:22 -0700

    Create prototype routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
+  resources :prototypes do
+      member do
+        get :select
+      end
+
+      collection do
+        get :available
+      end
+    end
 end
