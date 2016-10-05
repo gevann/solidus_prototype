9ffc2b3137899d5253e6e60d2da78ac3b416d504
Graeme Nathan <graeme@stembolt.com>
2016-10-05 13:40:38 -0700

    Create prototype factory spec

---
+++ spec/factories/prototype_factory_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe 'prototype factory' do
+  let(:factory_class) { Spree::Prototype }
+
+  describe 'plain prototype' do
+    let(:factory) { :prototype }
+
+    it_behaves_like 'a working factory'
+  end
+end
